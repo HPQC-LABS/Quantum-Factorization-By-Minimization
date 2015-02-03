ba3fb17edc575096a6cc2a631ec037720b574ba0
tanbur <richard.tanburn@hotmail.com>
2015-02-03 16:08:57 +0000

    Recursive Schaller qubit reduction method
    Reduce 4- to 3-qubit interactions using the recursive Schaller
    transformation

--- objective_function_helper.py
+++ objective_function_helper.py
@@ -7,8 +7,11 @@ Created on Wed Jan 07 14:35:08 2015
 
 from collections import defaultdict
 import itertools
+import operator
 
 import sympy
+
+from sympy_helper_fns import remove_binary_squares, expressions_to_variables
 
 
 
@@ -18,7 +21,7 @@ def _equations_to_groebner_exprs(eqns):
         the variables, and go
 
     '''
-    eqns = map(lambda eqn: eqn.lhs - eqn.rhs, eqns)
+    eqns = map(eqns_to_exprs, eqns)
     atoms = set.union(*[eqn.atoms(sympy.Symbol) for eqn in eqns])
 
     groebner_roots = [(atom*(1 - atom)).expand() for atom in atoms]
@@ -186,6 +189,10 @@ def sum_term_dicts(term_dicts):
             term_dict[term] += coef
     return term_dict
 
+def eqns_to_exprs(eqn):
+    ''' Take an equation, return lhs - rhs '''
+    return eqn.lhs - eqn.rhs
+
 def expressions_to_term_dict(exprs, process_eqn=None):
     ''' Given a list of Sympy expressions, calculate the term: coefficient dict
         of the objective function
@@ -281,19 +288,139 @@ def equations_to_sum_coef_str(eqns):
         <BLANKLINE>
         {x3: 7, c: 3, x2: 6, d: 4, x1: 5, a: 1, b: 2}
     '''
-    exprs = map(lambda x: x.lhs - x.rhs, eqns)
+    exprs = map(eqns_to_exprs, eqns)
     term_dict = expressions_to_term_dict(exprs)
     return term_dict_to_coef_string(term_dict)
 
+## Recursive Schaller method for reducing 4-qubit interactions to 3-qubit
+## ones
+def schaller_transform(a, b, s):
+    ''' Take a single 2-qubit interaction and a sum and return the new
+        Hamiltonian
+
+        >>> a, b, s, s1, s2 = sympy.symbols('a b s s1 s2')
+        >>> schaller_transform(a, b, s)
+        a*b + 2*a*s + 2*b*s + s
+
+        >>> schaller_transform(a, b, s1 + s2)
+        a*b + 2*a*s1 + 2*a*s2 + 2*b*s1 + 2*b*s2 - s1 - s2 + 2*(s1 + s2)**2
+    '''
+    return remove_binary_squares(a*b + (2*a*s).expand() + (2*b*s).expand() + 2*s**2 - s)
+
+def recursive_schaller_transform(expr):
+    ''' Take an equation and perform the recursive transform until we're only
+        left with 3-qubit interactions
+
+        >>> a, b, c, d, e, s, s1, s2 = sympy.symbols('a b c d e s s1 s2')
+
+        >>> recursive_schaller_transform((a*b + s) ** 2)
+        a*b + 2*a*s + 2*b*s + s
+        
+        >>> recursive_schaller_transform((a*b - s) ** 2)
+        a*b - 2*a*s - 2*b*s + 3*s
+
+        >>> recursive_schaller_transform((a*b + s1 + s2) ** 2)
+        a*b + 2*a*s1 + 2*a*s2 + 2*b*s1 + 2*b*s2 + 4*s1*s2 + s1 + s2
+        
+        Work when multiplying by a scalar
+        >>> recursive_schaller_transform(2*(a*b + s) ** 2)
+        2*a*b + 4*a*s + 4*b*s + 2*s
+        
+        Work when adding a scalar
+        >>> recursive_schaller_transform((a*b + s) ** 2 + 1)
+        a*b + 2*a*s + 2*b*s + s + 1
+
+        >>> recursive_schaller_transform((a*b + c*d + e - 2) ** 2)
+        a*b + 2*a*c*d + 2*a*e - 4*a + 2*b*c*d + 2*b*e - 4*b + c*d + 4*c*e - 8*c + 4*d*e - 8*d - 15*e + 22
+        
+        Intermediate stage of the above example after 1 iteration
+        Should be helpful for debugging
+        >>> intermed_stage = a*b + 2*a*c*d + 2*a*e - 4*a + 2*b*c*d + 2*b*e - 4*b - c*d - e + 2*(c*d + e - 2)**2 + 2
+        >>> recursive_schaller_transform(intermed_stage)
+        a*b + 2*a*c*d + 2*a*e - 4*a + 2*b*c*d + 2*b*e - 4*b + c*d + 4*c*e - 8*c + 4*d*e - 8*d - 15*e + 22
+    '''
+    return remove_binary_squares(_recursive_schaller_transform(expr))
+
+def _recursive_schaller_transform(expr):
+    ''' Recursive bit that actually does the work '''
+    expr_coef, expr = expr.as_coeff_Mul()   
+    
+    # If we have a squared thing, do all the funky stuff
+    if isinstance(expr, sympy.Pow):
+        # First substract off the power term so that we can add on the
+        # transformed stuff later
+        exp_expr, power = expr.args
+        
+        assert power == 2
+        
+        for term, coef in exp_expr.as_coefficients_dict().iteritems():
+            term_atoms = term.atoms(sympy.Symbol)
+            if len(term_atoms) > 1:
+                assert len(term_atoms) == 2
+                a = term_atoms.pop()
+                b = reduce(operator.mul, term_atoms, 1)
+                s = exp_expr - coef * term
+                transformed = schaller_transform(a, b, s)
+                return _recursive_schaller_transform(transformed) * expr_coef
+        return expr.expand()
+        
+    new_expr = 0
+    for term, coef in expr.as_coefficients_dict().iteritems():
+        if isinstance(term, sympy.Pow):
+            new_expr += coef * _recursive_schaller_transform(term)
+        else:
+            new_expr += coef * term
+    return new_expr
+            
+
+def equations_to_recursive_schaller_coef_str(eqns):
+    ''' Take equations and perform the recursive transform until we're only
+        left with 3-qubit interactions in the objective function
+
+        >>> a, b, c, d, e, s, s1, s2 = sympy.symbols('a b c d e s s1 s2')
+
+        >>> print equations_to_recursive_schaller_coef_str([sympy.Eq(a*b + s)])
+        3 1
+        1 2 1
+        2 3 2
+        1 3 2
+        <BLANKLINE>
+        {s: 3, b: 2, a: 1}
+
+        >>> print equations_to_recursive_schaller_coef_str([sympy.Eq(a*b + c*d + e, 2)])
+        1 2 1
+        2 3 4 2
+        1 5 2
+        1 3 4 2
+        3 -8
+        4 -8
+        22
+        3 5 4
+        2 5 2
+        1 -4
+        4 5 4
+        5 -15
+        2 -4
+        3 4 1
+        <BLANKLINE>
+        {c: 3, e: 5, b: 2, a: 1, d: 4}
+    '''
+    exprs = map(eqns_to_exprs, eqns)
+    exprs = [expr ** 2 for expr in exprs]
+    exprs = map(recursive_schaller_transform, exprs)
+    term_dict = expressions_to_term_dict(exprs)
+    coef_str = term_dict_to_coef_string(term_dict)
+    return coef_str
+    
 ## Introduce auxilary variables
-def schaller((ab, s)):
+def schaller_simple((ab, s)):
     ''' Change (ab-s)**2 to (ab - sa - sb + s), which has minimums at exactly
         the point ab = s
         
         >>> vars_ = sympy.symbols('a b s')
         >>> a, b, s = vars_
         >>> orig_expr = (a*b - s)**2
-        >>> schaller_expr = schaller((a*b, s))
+        >>> schaller_expr = schaller_simple((a*b, s))
         
         >>> print schaller_expr
         a*b - 2*a*s - 2*b*s + 3*s
@@ -306,6 +433,7 @@ def schaller((ab, s)):
         ...     else:
         ...         assert schaller_expr.subs(to_sub) > 0
     '''
+    assert len(ab.atoms(sympy.Symbol)) == 2
     a, b = ab.atoms(sympy.Symbol)
     return a*b - 2*a*s - 2*b*s + 3*s
 
@@ -345,7 +473,7 @@ def exprs_to_auxillary_term_dict(exprs):
     term_dict = sum_term_dicts(tds)
     
     # Now use the funky formula to provide equality for the auxillary variables
-    aux_exprs = map(schaller, aux_s.iteritems())
+    aux_exprs = map(schaller_simple, aux_s.iteritems())
     for term, coef in expressions_to_term_dict(aux_exprs).iteritems():     
         term_dict[term] += coef
     
