abdb6e39049f978aed0e6c6a8983b8f27e319d57
Richard Tanburn <tanburn@maths.ox.ac.uk>
2015-03-09 15:32:33 +0000

    Add shell scripts to git repo
     Changes to be committed:
    	new file:   copy_results.sh
    	new file:   print_results.sh

---
+++ print_results.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+SEARCH_STRING="End"
+#SEARCH_STRING="failed"
+
+echo "*** 20 ***"
+echo
+grep $SEARCH_STRING 20x20/* #-B 1
+echo
+echo "*** 30 ***"
+echo
+grep $SEARCH_STRING 30x30/* #-B 1
+echo
+echo "*** 40 ***"
+echo
+grep $SEARCH_STRING 40x40/* #-B 1
+echo
+echo "*** 50 ***"
+echo
+grep $SEARCH_STRING 50x50/* #-B 1
+echo
+echo "*** 60 ***"
+echo
+grep $SEARCH_STRING 60x60/* #-B 1
+echo
+echo "*** 70 ***"
+echo
+grep $SEARCH_STRING 70x70/* #-B 1
+echo
+echo "*** 80 ***"
+echo
+grep $SEARCH_STRING 80x80/* #-B 1
+echo
+echo "*** 90 ***"
+echo
+grep $SEARCH_STRING 90x90/* #-B 1
+echo
+echo "*** 100 ***"
+echo
+grep $SEARCH_STRING 100x100/* #-B 1
+echo
+echo "*** 110 ***"
+echo
+grep $SEARCH_STRING 110x110/* #-B 1
+echo
+echo "*** 120 ***"
+echo
+grep $SEARCH_STRING 120x120/* #-B 1
+echo
+echo "*** 130 ***"
+echo
+grep $SEARCH_STRING 130x130/* #-B 1
+echo
+echo "*** 140 ***"
+echo
+grep $SEARCH_STRING 140x140/* #-B 1
+echo
+echo "*** 150 ***"
+echo
+grep $SEARCH_STRING 150x150/* #-B 1
+echo
+echo "*** 160 ***"
+echo
+grep $SEARCH_STRING 160x160/* #-B 1
+echo
+echo "*** 170 ***"
+echo
+grep $SEARCH_STRING 170x170/* #-B 1
+echo
+echo "*** 180 ***"
+echo
+grep $SEARCH_STRING 180x180/* #-B 1
+echo
+echo "*** 190 ***"
+echo
+grep $SEARCH_STRING 190x190/* #-B 1
+
