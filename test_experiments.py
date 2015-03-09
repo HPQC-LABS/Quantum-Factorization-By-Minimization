4efc30382aed09da5a5d53abe0401daac462c345
tanbur <richard.tanburn@hotmail.com>
2015-03-09 15:14:25 +0000

    Tests for experiments in the cfg file
    Run a small batch of experiments and compare the final number of qubits
    to the expected number in the cfg file

---
+++ test_experiments.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 21 19:48:44 2015
+
+@author: Richard
+"""
+
+#!/usr/bin/env python
+
+
+from time import time
+
+from cfg_sympy_solver import (EXPERIMENTS, EXTENDED_EXPERIMENTS, EXPERIMENTS_21,
+                              QUBIT_REDUCTION_ID)
+
+from sympy_solver import EquationSolver
+from verification import check_solutions
+
+
+SKIP = [100]
+for exp, params in sorted(EXTENDED_EXPERIMENTS.iteritems()):
+    try:
+        
+        if exp in SKIP:
+            continue
+    
+        # A default experiment to run
+        digitsInMultiplicand1, digitsInMultiplicand2, product, expected_qubits = params
+    #    digitsInMultiplicand1, digitsInMultiplicand2, product = params
+        
+        # Default assumption parameters
+        # Use standard carry equations by default
+        qubit_reduction_method = 0
+        
+        equation_generator, coef_str_generator = QUBIT_REDUCTION_ID[qubit_reduction_method]
+        
+        eqns = equation_generator(digitsInMultiplicand1, digitsInMultiplicand2, product)
+        
+        s = time()
+        # None means the result will be printed to screen
+        output = None#OutputFileName
+        
+        
+        # We can use the handy state caching    
+        cache_name = None#'_state_{}'.format(str(product)[-6:])
+        log_deductions = False
+        invariant_interactions_on_substitution = True
+    
+        system = EquationSolver(eqns, output_filename=output, 
+                                            log_deductions=log_deductions,
+                                            invariant_interactions_on_substitution=invariant_interactions_on_substitution)
+        system.solve_equations(verbose=False, max_iter=100)
+            
+        print '***\tExperiment {}\t***'.format(exp)
+    #    print 'Final Equations'
+    #    for e in system.final_equations:
+    #        print e
+    
+        print 'Num Qubits Start: {}'.format(system.num_qubits_start)
+        print 'Num Qubits End: {}'.format(len(system.unsolved_var))
+        print 'Num Qubits Expected: {}'.format(expected_qubits)
+        print '\nSolved in {:.3f}s'.format(time() - s)
+        
+        check_solutions(product, system.solutions.copy(), verbose=True)
+
+    except Exception as e:
+        print '***\tExperiment {}\t***'.format(exp)
+        print e
+    
+    print '\n' * 2
