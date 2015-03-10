13b82aa2d861d923b72356476020cf034750cff1
tanbur <richard.tanburn@hotmail.com>
2015-03-10 19:40:47 +0000

    Automatically generate all shell scripts

--- copy_results.sh
+++ copy_results.sh
@@ -2,41 +2,28 @@
 
 cd /home/tanburn/Quantum-Factorization-By-Minimization/
 
-echo "*** 20 ***"
 cp -r 20x20/ ../public_html/
-echo "*** 30 ***"
 cp -r 30x30/ ../public_html/
-echo "*** 40 ***"
 cp -r 40x40/ ../public_html/
-echo "*** 50 ***"
 cp -r 50x50/ ../public_html/
-echo "*** 60 ***"
 cp -r 60x60/ ../public_html/
-echo "*** 70 ***"
 cp -r 70x70/ ../public_html/
-echo "*** 80 ***"
 cp -r 80x80/ ../public_html/
-echo "*** 90 ***"
 cp -r 90x90/ ../public_html/
-echo "*** 100 ***"
 cp -r 100x100/ ../public_html/
-echo "*** 110 ***"
 cp -r 110x110/ ../public_html/
-echo "*** 120 ***"
 cp -r 120x120/ ../public_html/
-echo "*** 130 ***"
 cp -r 130x130/ ../public_html/
-echo "*** 140 ***"
 cp -r 140x140/ ../public_html/
-echo "*** 150 ***"
 cp -r 150x150/ ../public_html/
-echo "*** 160 ***"
 cp -r 160x160/ ../public_html/
-echo "*** 170 ***"
 cp -r 170x170/ ../public_html/
-echo "*** 180 ***"
 cp -r 180x180/ ../public_html/
-echo "*** 190 ***"
 cp -r 190x190/ ../public_html/
-
+cp -r 200x200/ ../public_html/
+cp -r 210x210/ ../public_html/
+cp -r 220x220/ ../public_html/
+cp -r 230x230/ ../public_html/
+cp -r 240x240/ ../public_html/
+cp -r 250x250/ ../public_html/
 ./print_results.sh > ../public_html/results_summary.txt
--- ut_generate_batch_script.py
+++ ut_generate_batch_script.py
@@ -9,43 +9,93 @@ import os
 
 from PrimeFactorizationEquationGenerator import num_to_factor_num_qubit
 
-# Check the batch_scipts folder exists
 BATCH_FOLDER = 'batch_scripts'
-if not os.path.exists(BATCH_FOLDER):
-    os.makedirs(BATCH_FOLDER)
-
 input_filename = 'large_semiprimes.txt'
-nums = open(input_filename, 'r').read()
 
-# Turn the text into numbers
-nums = map(int, nums.split('\n'))
+DIMENSIONS = range(20, 260, 10)
 
-# This holds the lines we want to print
-lines = []
+PRINT_RESULTS_FILENAME = 'print_results.sh'
+COPY_RESULTS_FILENAME = 'copy_results.sh'
 
-# Used to count the position of the prime in its category
-count = 0
-prev_num_digit = None
+BASH_STR = '#!/bin/sh'
 
-# f is the file object
-f = None
-for num in nums:
-    num_d1, num_d2 = num_to_factor_num_qubit(num)
-    if (num_d1, num_d2) != prev_num_digit:
-        if lines:
-            filename = 'batch_experiments_{}x{}.sh'.format(*prev_num_digit)
-            filename = os.path.join(BATCH_FOLDER, filename)
-            f = open(filename, 'w')
-            f.write('#!/bin/bash\n\n')
-            f.write('cd /home/tanburn/Quantum-Factorization-By-Minimization/\n\n')
-            f.writelines(lines)
-            f.close()
-            lines = []
-        count = 1
-        prev_num_digit = (num_d1, num_d2)
-        lines.append('\n\n## {}x{} semiprimes\n\n'.format(num_d1, num_d2))
-    else:
-        count += 1
-    count_str = str(count).rjust(3, '0')
-    entry_str = 'python ../PrimeFactorizationEquationGenerator.py {prod} > ../{num_d1}x{num_d2}/{num_d1}x{num_d2}_{count}_0.txt\n'.format(prod=num, num_d1=num_d1, num_d2=num_d2, count=count_str)
-    lines.append(entry_str)
+HOME_DIR = '/home/tanburn/Quantum-Factorization-By-Minimization/'
+
+def generate_batch_scripts():
+# Check the batch_scipts folder exists
+    if not os.path.exists(BATCH_FOLDER):
+        os.makedirs(BATCH_FOLDER)
+    
+    nums = open(input_filename, 'r').read()
+    
+    # Turn the text into numbers
+    nums = map(int, nums.split('\n'))
+    
+    # This holds the lines we want to print
+    lines = []
+    
+    # Used to count the position of the prime in its category
+    count = 0
+    prev_num_digit = None
+    
+    # f is the file object
+    f = None
+    for num in nums:
+        num_d1, num_d2 = num_to_factor_num_qubit(num)
+        if (num_d1, num_d2) != prev_num_digit:
+            if lines:
+                filename = 'batch_experiments_{}x{}.sh'.format(*prev_num_digit)
+                filename = os.path.join(BATCH_FOLDER, filename)
+                f = open(filename, 'w')
+                f.write(BASH_STR + '\n\n')
+                f.write('cd {HOME_DIR}\n\n'.format(HOME_DIR=HOME_DIR))
+                f.writelines(lines)
+                f.close()
+                lines = []
+            count = 1
+            prev_num_digit = (num_d1, num_d2)
+            lines.append('\n\n## {}x{} semiprimes\n\n'.format(num_d1, num_d2))
+        else:
+            count += 1
+        count_str = str(count).rjust(3, '0')
+        entry_str = 'python PrimeFactorizationEquationGenerator.py {prod} > {num_d1}x{num_d2}/{num_d1}x{num_d2}_{count}_0.txt\n'.format(prod=num, num_d1=num_d1, num_d2=num_d2, count=count_str)
+        lines.append(entry_str)
+
+    return True
+
+def generate_print_results(dimensions):
+    ''' Generate the script that puts the grep output into a file in a given folder '''
+    f = open(PRINT_RESULTS_FILENAME, 'w')
+    f.write(BASH_STR + '\n\n')
+
+    f.write('cd {HOME_DIR}\n\n'.format(HOME_DIR=HOME_DIR))
+
+    f.write('SEARCH_STRING="End"\n\n')
+
+    template = '''echo "*** {dim} ***"\necho\ngrep $SEARCH_STRING {dim}x{dim}/*\necho\n'''
+    for dim in dimensions:
+        f.write(template.format(dim=dim))
+    f.close()
+
+def generate_copy_results(dimensions):
+    ''' Generate the script that copies the grep output into 
+        the public_html folder    
+    '''
+    f = open(COPY_RESULTS_FILENAME, 'w')
+    f.write(BASH_STR + '\n\n')
+    f.write('cd {HOME_DIR}\n\n'.format(HOME_DIR=HOME_DIR))
+
+    template = '''cp -r {dim}x{dim}/ ../public_html/\n'''
+    for dim in dimensions:
+        f.write(template.format(dim=dim))
+
+    f.write('./{print_res_file} > ../public_html/results_summary.txt\n'.format(print_res_file=PRINT_RESULTS_FILENAME))
+    
+    f.close()
+
+
+if __name__ == '__main__':
+    generate_batch_scripts()
+    generate_print_results(DIMENSIONS)
+    generate_copy_results(DIMENSIONS)
+    
