6e286e5fc6375defb7688061ed55f890aaf5493c
tanbur <richard.tanburn@hotmail.com>
2015-03-10 21:38:59 +0000

    Initial commit

---
+++ sympy_paralleliser.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 21:09:13 2015
+
+@author: Richard
+"""
+
+import itertools
+import multiprocessing
+
+DEFAULT_PROCESSES = 5
+
+def get_pool(processes=DEFAULT_PROCESSES):
+    ''' Return a pool object '''
+    return multiprocessing.Pool(processes=processes)
+
+def _subs_wrap((eqn, sub_tuple)):
+    ''' Wrapper function to allow parallelisation '''
+    return eqn.subs(sub_tuple)
+
+def paralellised_subs(equations, to_sub, processes=DEFAULT_PROCESSES, pool=None):
+    ''' Take a list of equations and a substitution dict and parallelise the
+        substitution.
+        If a pool is given, ignore processes
+    '''
+    if isinstance(to_sub, dict):
+        to_sub = to_sub.items()
+    
+    if pool is None:
+        pool = get_pool(processes=processes)
+    sub_copies = itertools.repeat(to_sub, len(equations))
+    result = pool.map(_subs_wrap, zip(equations, sub_copies))
+    return result
