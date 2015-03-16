245c67af478cf2a6f746aa0c01dd1eb5f4920820
tanbur <richard.tanburn@hotmail.com>
2015-03-16 18:51:49 +0000

    Refer to the default experiment properly

--- PrimeFactorizationEquationGenerator.py
+++ PrimeFactorizationEquationGenerator.py
@@ -150,7 +150,7 @@ if __name__ == '__main__':
         else:
             import doctest
             doctest.testmod()
-            product = 143
+            product = EXPERIMENTS[1].product
             factorize(product)
     except Exception as e:
         print e
