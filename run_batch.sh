862d7f4fbd0fc09379ac918f016a874f4a675b38
tanbur <richard.tanburn@hotmail.com>
2015-03-16 18:06:55 +0000

    Utility to run batch scripts easily
    New utility for running a batch in a new screen session

---
+++ run_batch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /home/tanburn/Quantum-Factorization-By-Minimization/batch_scripts
+
+
+if ["$1" == ""]; then
+    exit 1
+fi
+
+EXP_NAME="exp$1"
+
+screen -S $EXP_NAME -d -m
+
+BATCH_FILE="batch_experiments_$1x$1.sh"
+
+screen -S $EXP_NAME -X stuff ". ./$BATCH_FILE\r"
