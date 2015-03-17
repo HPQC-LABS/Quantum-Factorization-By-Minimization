0c66f2966787cb9073c9aec13b2397ad3bdf334e
tanbur <richard.tanburn@hotmail.com>
2015-03-17 19:31:56 +0000

    Update expected qubits for most recent solver

--- cfg_sympy_solver.py
+++ cfg_sympy_solver.py
@@ -34,7 +34,7 @@ Experiment = namedtuple('Experiment', ('digits_multiplicand_1',
 
 EXPERIMENTS = {
     1: Experiment(4, 4, 143, 1),
-    2: Experiment(8, 8, 56153, 36), # 38 qubits to go
+    2: Experiment(8, 8, 56153, 35),
     3: Experiment(17, 17, 4299161663, 1),
     4: Experiment(21, 21, 1099532599387, 1),
     5: Experiment(24, 24, 70368895172689, 0),
@@ -45,12 +45,12 @@ EXPERIMENTS = {
     10: Experiment(17, 17, 4297981997, 1),
     11: Experiment(17, 17, 4301127773, 1),
     12: Experiment(21, 21, 1099526307889, 0),
-    13: Experiment(21, 21, 1099551473989, 8), # 10 qubits to go
-    14: Experiment(21, 21, 1099585029317, 1), # 8 qubits to go
-    15: Experiment(45, 45, 309485009822787627980424653, 12),
+    13: Experiment(21, 21, 1099551473989, 1),
+    14: Experiment(21, 21, 1099585029317, 1),
+    15: Experiment(45, 45, 309485009822787627980424653, 1),
     16: Experiment(45, 45, 309485009821943203050291389, 1),
     17: Experiment(51, 51, 1267650600228508624673600186743, 1),
-    18: Experiment(51, 51, 1267650600228402790082356974917, 10),
+    18: Experiment(51, 51, 1267650600228402790082356974917, 1),
     100: Experiment(165, 165, 1522605027922533360535618378132637429718068114961380688657908494580122963258952897654000350692006139, -1),
 }    
 
@@ -66,28 +66,28 @@ PRESENTS = {
 }
 
 EXPERIMENTS_21 = {
-    1: Experiment(21, 21, 1099526307889, 0), # 0 qubits to go
-    2: Experiment(21, 21, 1099551473989, 10), # 10 qubits to go
-    3: Experiment(21, 21, 1099585029317, 8), # 8 qubits to go
-    4: Experiment(21, 21, 1099589223769, 6), # 6 qubits to go
-    5: Experiment(21, 21, 1099597612433, 4), # 4 qubits to go
-    6: Experiment(21, 21, 1099618585129, 16), # 16 qubits to go
-    7: Experiment(21, 21, 1099635361109, 11), # 11 qubits to go
-    8: Experiment(21, 21, 1099639557193, 15), # 15 qubits to go
-    9: Experiment(21, 21, 1099664721601, 18), # 18 qubits to go
-    10: Experiment(21, 21, 1099677306109, 7), # 7 qubits to go
-    11: Experiment(21, 21, 1099698279521, 21), # 21
-    12: Experiment(21, 21, 1099723448393, 15), # 15
-    13: Experiment(21, 21, 1099731839761, 7), # 7
-    14: Experiment(21, 21, 1099740228649, 23), # 23
-    15: Experiment(21, 21, 1099748617937, 14), # 14
-    16: Experiment(21, 21, 1099752803077, 15), # 15
-    17: Experiment(21, 21, 1099752812581, 15), # 15
-    18: Experiment(21, 21, 1099769592277, 20), # 20
-    19: Experiment(21, 21, 1099777982209, 27), # 27
-    20: Experiment(21, 21, 1099811527037, 8), # 8
-    21: Experiment(21, 21, 1099819917733, 7), # 7
-    22: Experiment(21, 21, 1099874457257, 28), # 28
+    1: Experiment(21, 21, 1099526307889, 0),
+    2: Experiment(21, 21, 1099551473989, 1),
+    3: Experiment(21, 21, 1099585029317, 1),
+    4: Experiment(21, 21, 1099589223769, 0),
+    5: Experiment(21, 21, 1099597612433, 1),
+    6: Experiment(21, 21, 1099618585129, 0),
+    7: Experiment(21, 21, 1099635361109, 1),
+    8: Experiment(21, 21, 1099639557193, 1),
+    9: Experiment(21, 21, 1099664721601, 1),
+    10: Experiment(21, 21, 1099677306109, 1),
+    11: Experiment(21, 21, 1099698279521, 1),
+    12: Experiment(21, 21, 1099723448393, 1),
+    13: Experiment(21, 21, 1099731839761, 0),
+    14: Experiment(21, 21, 1099740228649, 5),
+    15: Experiment(21, 21, 1099748617937, 1),
+    16: Experiment(21, 21, 1099752803077, 1),
+    17: Experiment(21, 21, 1099752812581, 1),
+    18: Experiment(21, 21, 1099769592277, 1),
+    19: Experiment(21, 21, 1099777982209, 21),
+    20: Experiment(21, 21, 1099811527037, 1),
+    21: Experiment(21, 21, 1099819917733, 1),
+    22: Experiment(21, 21, 1099874457257, 1),
 }
 
 EXTENDED_EXPERIMENTS = EXPERIMENTS.copy()
