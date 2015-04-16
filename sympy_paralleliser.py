9c3f9d7c55e96e9e596f938a755df055faded3b8
tanbur <richard.tanburn@hotmail.com>
2015-04-17 00:03:39 +0100

    Initial commit of parallelised solve_equations
    This might not work until pickling is resolved

--- sympy_paralleliser.py
+++ sympy_paralleliser.py
@@ -10,7 +10,9 @@ import multiprocessing
 import sys
 
 from sympy.core.cache import clear_cache
+
 from sympy_subs import subs_many
+from contradiction_exception import ContradictionException
 
 DEFAULT_NUM_PROCESSES = {'linux': 6,
                          'linux2': 6,
@@ -28,7 +30,16 @@ def _subs_wrap((eqns, sub_tuple)):
     subbed = subs_many(eqns, sub_tuple)
     clear_cache()
     return subbed
-    
+
+def _solve_equations_wrap(solver):
+    # Make sure we don't parallelise again
+    solver.close_pool()
+    solver.parallelise = False
+    try:
+        solver.solve_equations()
+    except ContradictionException:
+        return None
+    return solver
 
 def paralellised_subs(equations, to_sub, processes=DEFAULT_NUM_PROCESSES, pool=None):
     ''' Take a list of equations and a substitution dict and parallelise the
@@ -66,9 +77,54 @@ def paralellised_subs(equations, to_sub, processes=DEFAULT_NUM_PROCESSES, pool=N
         pool.join()
         pool = None
     return result
+
+def paralellised_solve_equations(solvers, processes=DEFAULT_NUM_PROCESSES, pool=None):
+    ''' Take a list of solvers and call each of their solve_equations methods
+        If a pool is given, ignore processes
+
+        >>> prod = 143
+        >>> dim1, dim2 = num_to_factor_num_qubit(prod)
+        >>> eqns = generate_carry_equations(dim1, dim2, prod)
+        >>> p1, q1 = sympy.symbols('p1 q1')
+        >>> solvers = []
+        >>> for pv, qv in itertools.product(range(2), repeat=2):
+        ...     solver = SOLVER(eqns)
+        ...     solver.add_solution(p1, pv)
+        ...     solver.add_solution(q1, qv)
+        ...     solvers.append(solver)
+        >>> solvers = paralellised_solve_equations(solvers)
+        >>> for solver in solvers:
+        ...     if solver is None:
+        ...         print None
+        ...     else:
+        ...         print len(solver.unsolved_var)
+        None
+        0
+        0
+        None
+    '''
+    if pool is None:
+        pool = get_pool(processes=processes)
+        close_on_exit = True
+    else:
+        close_on_exit = False
+
+    result = pool.map(_solve_equations_wrap, solvers)
+
+    # If we made the pool, shut it down again
+    if close_on_exit:
+        pool.close()
+        pool.join()
+        pool = None
+    return result
     
 if __name__ == "__main__":
     import doctest
     import sympy
+    import itertools
     from sympy_helper_fns import balance_terms
+    from carry_equations_generator import generate_carry_equations
+    from semiprime_tools import num_to_factor_num_qubit
+    from solver_hybrid import SolverHybrid
+    SOLVER = SolverHybrid
     doctest.testmod()
