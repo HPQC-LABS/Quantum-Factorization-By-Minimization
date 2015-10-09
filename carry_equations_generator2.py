8f1f522f73f68bb4d053fc8c1a381f9a323cc404
tanbur <richard.tanburn@hotmail.com>
2015-10-09 15:07:32 +0100

    Auxiliary equation generator
    Add functionality to replace every p_i*q_j interaction with a m_i_j
    variable, eliminating all cubic and quartic interactions

--- carry_equations_generator2.py
+++ carry_equations_generator2.py
@@ -182,7 +182,107 @@ def generate_factorisation_equation(num_dig1=None, num_dig2=None, product=None):
 
     return sympy.Eq(eqn_lhs, eqn_rhs)
 
+def generate_carry_equations_auxiliary(num_dig1=None, num_dig2=None, product=None):
+    ''' Given a product, generate the carry equations that express this
+        using auxiliary variables for the p_i * q_j interactions
 
+        >>> product = 25
+        >>> eqns = generate_carry_equations_auxiliary(product=product)
+        >>> for e in eqns: print e
+        m0_0 == 1
+        m0_1 + m1_0 == 2*z12
+        m0_2 + m1_1 + m2_0 + z12 == 2*z23 + 4*z24
+        m1_2 + m2_1 + z23 == 2*z34 + 1
+        m2_2 + z24 + z34 == 2*z45 + 1
+        z45 == 0
+        p0*q0 == m0_0
+        p0*q1 == m0_1
+        p0*q2 == m0_2
+        p1*q0 == m1_0
+        p1*q1 == m1_1
+        p1*q2 == m1_2
+        p2*q0 == m2_0
+        p2*q1 == m2_1
+        p2*q2 == m2_2
+
+        >>> product = 143
+        >>> eqns = generate_carry_equations_auxiliary(product=product)
+        >>> for e in eqns: print e
+        m0_0 == 1
+        m0_1 + m1_0 == 2*z12 + 1
+        m0_2 + m1_1 + m2_0 + z12 == 2*z23 + 4*z24 + 1
+        m0_3 + m1_2 + m2_1 + m3_0 + z23 == 2*z34 + 4*z35 + 1
+        m1_3 + m2_2 + m3_1 + z24 + z34 == 2*z45 + 4*z46
+        m2_3 + m3_2 + z35 + z45 == 2*z56 + 4*z57
+        m3_3 + z46 + z56 == 2*z67
+        z57 + z67 == 2*z78 + 1
+        z78 == 0
+        p0*q0 == m0_0
+        p0*q1 == m0_1
+        p0*q2 == m0_2
+        p0*q3 == m0_3
+        p1*q0 == m1_0
+        p1*q1 == m1_1
+        p1*q2 == m1_2
+        p1*q3 == m1_3
+        p2*q0 == m2_0
+        p2*q1 == m2_1
+        p2*q2 == m2_2
+        p2*q3 == m2_3
+        p3*q0 == m3_0
+        p3*q1 == m3_1
+        p3*q2 == m3_2
+        p3*q3 == m3_3
+    '''
+    if product is None:
+        raise ValueError('generate_carry_equations must be given a product')
+    if num_dig1 is None:
+        assert num_dig2 is None
+        num_dig1, num_dig2 = num_to_factor_num_qubit(product)
+
+    eqns_rhs = [int(digit) for digit in bin(product)[2:][::-1]]
+    eqns_lhs = [0 for _ in eqns_rhs]
+
+    # Create a holder for the equations that constrain pi.qj=mi_j
+    constraints = []
+
+    # Now pad them
+    for i in xrange(5):
+        eqns_lhs.append(0)
+        eqns_rhs.append(0)
+
+    ## Now add the contributions from the actual factors
+    for pi in xrange(num_dig1):
+        pi_str = 'p{}'.format(pi)
+        for qi in xrange(num_dig2):
+            qi_str = 'q{}'.format(qi)
+
+            # Add the single interaction variable
+            interaction_str = 'm{}_{}'.format(pi, qi)
+            interaction_var = sympy.sympify(interaction_str)
+            eqns_lhs[pi + qi] += interaction_var
+
+            constraint = sympy.Eq(sympy.sympify('{}*{}'.format(pi_str, qi_str)),
+                                  interaction_var)
+            constraints.append(constraint)
+
+
+    ## Now loop over and add the carry variables
+    for column_ind, sum_ in enumerate(eqns_lhs):
+        if sum_ == 0:
+            max_val = 1
+        else:
+            max_val = max_value(sum_)
+        max_pow_2 = int(math.floor(math.log(max_val, 2)))
+        for i in xrange(1, max_pow_2 + 1):
+            z = sympy.Symbol('z{}{}'.format(column_ind, column_ind + i))
+            eqns_rhs[column_ind] += (2 ** i) * z
+            eqns_lhs[column_ind + i] += z
+
+    eqns = [sympy.Eq(lhs, rhs) for lhs, rhs in zip(eqns_lhs, eqns_rhs)]
+    eqns = filter(is_equation, eqns)
+
+    return eqns + constraints
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
